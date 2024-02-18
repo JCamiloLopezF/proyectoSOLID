7ec46ce402278a2afee0f668f909c0ca60b19917
Juan Pablo López <juanp.lopezg@uqvirtual.edu.co>
2024-02-18 16:21:45 -0500

    agreagcion del DIP

---
+++ proyecto_solid/src/main/java/co/edu/uniquindio/edu/co/DIP/Buena Practica/file.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.edu.co.DIP.Buena Practica;
+
+public class file {
+    
+}
---
+++ proyecto_solid/src/main/java/co/edu/uniquindio/edu/co/DIP/Mala Practica/file.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.edu.co.DIP.Mala Practica;
+
+public class file {
+    
+}
